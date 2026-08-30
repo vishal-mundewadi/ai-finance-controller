ef08011d49a0d8f1b905d3301adb949cdbc5f92a
Vishal Mundewadi <vishalmundewadi226@gmail.com>
2026-08-30 17:59:43 +0530

    frontend scaffolded with vite+react+typescript,tailwind and framer motion installed and working

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-black">Tailwind is working</h1>
+    </div>
+  )
+}
+
+export default App
